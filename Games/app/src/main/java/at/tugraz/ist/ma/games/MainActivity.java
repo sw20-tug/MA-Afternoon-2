86e90f87b99acd45cceefed3fd915af23e3f4d12
Stefan di Vora <stefan.divora@a1.net>
2020-04-01 14:43:49 +0200

    GAMES_001 Updated Welcome Screen UI [DS, RA]

--- Games/app/src/main/java/at/tugraz/ist/ma/games/MainActivity.java
+++ Games/app/src/main/java/at/tugraz/ist/ma/games/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnTicTacToe = findViewById(R.id.btnTicTacToe);
+        ImageButton btnDontTouch = findViewById(R.id.btnDontTouch);
+        ImageButton btnSettings = findViewById(R.id.btnSettings);
+        ImageButton btnHangman = findViewById(R.id.btnHangman);
+
+        btnTicTacToe.setOnClickListener(v -> btnTicTacToe_Click());
+        btnHangman.setOnClickListener(v -> btnHangman_Click());
+        btnSettings.setOnClickListener(v -> btnSettings_Click());
+        btnDontTouch.setOnClickListener(v -> btnDontTouch_Click());
     }
+
+    private void btnTicTacToe_Click() {
+        Intent intentTTTSettings = new Intent(getApplicationContext(), TicTacToeSettingsActivity.class);
+        startActivity(intentTTTSettings);
+    }
+
+    private void btnHangman_Click() {
+
+    }
+
+    private void btnSettings_Click() {
+
+    }
+
+    private void btnDontTouch_Click() {
+
+    }
+
 }
