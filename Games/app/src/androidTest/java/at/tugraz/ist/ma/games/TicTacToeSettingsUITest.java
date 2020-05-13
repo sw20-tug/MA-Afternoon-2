2f4b240068854c3ee7f3a94db034094cd289e743
Stefan di Vora <stefan.divora@a1.net>
2020-05-13 14:48:15 +0200

    GAMES_015 Adapt UI tests [DS,KM]

--- Games/app/src/androidTest/java/at/tugraz/ist/ma/games/TicTacToeSettingsUITest.java
+++ Games/app/src/androidTest/java/at/tugraz/ist/ma/games/TicTacToeSettingsUITest.java
@@ -1,6 +1,7 @@
 package at.tugraz.ist.ma.games;
 
 import android.content.Context;
+import android.widget.SeekBar;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -13,8 +14,13 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withTagValue;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -38,9 +44,183 @@ public class TicTacToeSettingsUITest {
         assertEquals("at.tugraz.ist.ma.games", appContext.getPackageName());
     }
 
+    public void checkInitTextViews() {
+        onView(withId(R.id.ttt_tv_PlayerOneSymbol)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_tv_PlayerTwoSymbol)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_tv_GameMode)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_tv_TicTacToe)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_tv_AIDifficulty)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.ttt_tv_PlayerOneSymbol)).check(matches(withText(R.string.player_one)));
+        onView(withId(R.id.ttt_tv_PlayerTwoSymbol)).check(matches(withText(R.string.player_two)));
+        onView(withId(R.id.ttt_tv_GameMode)).check(matches(withText(R.string.GameMode)));
+        onView(withId(R.id.ttt_tv_TicTacToe)).check(matches(withText(R.string.GameMode_TicTacToe)));
+        onView(withId(R.id.ttt_tv_AIDifficulty)).check(matches(withText(R.string.AIDifficulty)));
+    }
+
     @Test
     public void InterfaceUITest()
     {
+        checkInitTextViews();
+
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_btn_play)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_sb_AIDifficulty)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(withText(R.string.GameModePvP)));
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_X)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(withText(R.string.TTT_O)));
+
+        onView(withId(R.id.ttt_btn_play)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_btn_play)).check(matches(withText(R.string.play)));
+
+        onView(withId(R.id.ttt_sb_AIDifficulty)).check(matches(isAssignableFrom(SeekBar.class)));
     }
 
+    @Test
+    public void performStartGame() {
+        onView(withId(R.id.ttt_btn_play)).perform(click());
+        intended(hasComponent(TicTacToeActivity.class.getName()));
+    }
+
+    @Test
+    public void changeGameModeToPVE() {
+        onView(withId(R.id.ttt_tb_GameMode)).perform(click());
+
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(withText(R.string.GameModePvAI)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void changeGameModeMultipleTimes() {
+        onView(withId(R.id.ttt_tb_GameMode)).perform(click());
+
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(withText(R.string.GameModePvAI)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.ttt_tb_GameMode)).perform(click());
+
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(withText(R.string.GameModePvP)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void changePlayerOnePVP() {
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_O)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(withText(R.string.TTT_X)));
+    }
+
+    @Test
+    public void changePlayerTwoPVP() {
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_O)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(withText(R.string.TTT_X)));
+    }
+
+    @Test
+    public void changePlayerPVPMutipleTimes() {
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_O)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(withText(R.string.TTT_X)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_X)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(withText(R.string.TTT_O)));
+    }
+
+    @Test
+    public void changePlayerOnePVE() {
+        onView(withId(R.id.ttt_tb_GameMode)).perform(click());
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(withText(R.string.GameModePvAI)));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_O)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void changePlayerOnePVEMultipleTimes() {
+        onView(withId(R.id.ttt_tb_GameMode)).perform(click());
+        onView(withId(R.id.ttt_tb_GameMode)).check(matches(withText(R.string.GameModePvAI)));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_O)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_X)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void changeMultipleSettingProperties() {
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).perform(click());
+        onView(withId(R.id.ttt_tb_GameMode)).perform(click());
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isClickable()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(isNotChecked()));
+        onView(withId(R.id.ttt_tb_PlayerOneSymbol)).check(matches(withText(R.string.TTT_X)));
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.ttt_tb_GameMode)).perform(click());
+
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(isDisplayed()));
+        onView(withId(R.id.ttt_tb_PlayerTwoSymbol)).check(matches(withText(R.string.TTT_O)));
+    }
 }
