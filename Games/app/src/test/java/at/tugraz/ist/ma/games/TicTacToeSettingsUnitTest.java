5c1fe7ff53517bc65b21dfdab39fef7e6d44fd43
Stefan di Vora <stefan.divora@a1.net>
2020-05-13 13:55:12 +0200

    GAMES_015 Adapt unit tests [DS,KM]

--- Games/app/src/test/java/at/tugraz/ist/ma/games/TicTacToeSettingsUnitTest.java
+++ Games/app/src/test/java/at/tugraz/ist/ma/games/TicTacToeSettingsUnitTest.java
@@ -22,5 +22,37 @@ public class TicTacToeSettingsUnitTest {
     {
         ticTacToeSettings = new TicTacToeSettings();
     }
+
+    @Test
+    public void checkDefaultSettingsTest() {
+        assertEquals(ticTacToeSettings.getAiDifficulty(), TicTacToeSettings.AI_Difficulty.EASY);
+        assertEquals(ticTacToeSettings.getGameMode(), TicTacToeSettings.GAME_MODE.PVP);
+        assertEquals(ticTacToeSettings.getStartingPlayer(), TicTacToe.Tile.CROSS);
+    }
+
+    @Test
+    public void checkSpecificSettingsTest() {
+        TicTacToeSettings ticTacToeSettings_spec = new TicTacToeSettings(TicTacToeSettings.AI_Difficulty.HARD, TicTacToeSettings.GAME_MODE.PVE, TicTacToe.Tile.CIRCLE);
+
+        assertEquals(ticTacToeSettings_spec.getAiDifficulty(), TicTacToeSettings.AI_Difficulty.HARD);
+        assertEquals(ticTacToeSettings_spec.getGameMode(), TicTacToeSettings.GAME_MODE.PVE);
+        assertEquals(ticTacToeSettings_spec.getStartingPlayer(), TicTacToe.Tile.CIRCLE);
+    }
+
+    @Test
+    public void checkChangeSettingsTest() {
+        assertEquals(ticTacToeSettings.getAiDifficulty(), TicTacToeSettings.AI_Difficulty.EASY);
+        assertEquals(ticTacToeSettings.getGameMode(), TicTacToeSettings.GAME_MODE.PVP);
+        assertEquals(ticTacToeSettings.getStartingPlayer(), TicTacToe.Tile.CROSS);
+
+        ticTacToeSettings.setAiDifficulty(TicTacToeSettings.AI_Difficulty.HARD);
+        ticTacToeSettings.setGameMode(TicTacToeSettings.GAME_MODE.PVE);
+        ticTacToeSettings.setStartingPlayer(TicTacToe.Tile.CIRCLE);
+
+        assertEquals(ticTacToeSettings.getAiDifficulty(), TicTacToeSettings.AI_Difficulty.HARD);
+        assertEquals(ticTacToeSettings.getGameMode(), TicTacToeSettings.GAME_MODE.PVE);
+        assertEquals(ticTacToeSettings.getStartingPlayer(), TicTacToe.Tile.CIRCLE);
+    }
+
 }
 
