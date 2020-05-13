ad06a5e1575c3728c2e7ff62ad034a1f2f15fb4e
Daniel Pollhammer <pollhammer@student.tugraz.at>
2020-05-13 14:10:57 +0200

    GAMES_011_B unit tests and white tile logic first draft [PD,RA]

---
+++ Games/app/src/main/java/at/tugraz/ist/ma/games/WhiteTiles.java
@@ -0,0 +1,86 @@
+package at.tugraz.ist.ma.games;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+
+
+public class WhiteTiles
+{
+    public enum TileColor {
+        BLACK,
+        WHITE
+    }
+    ArrayList<Button> buttons = new ArrayList<Button>();
+    ArrayList<TileColor> button_state = new ArrayList<TileColor>();
+    private int player_points = 0;
+    private boolean debug_mode = false;
+
+    public WhiteTiles(boolean debug_mode)
+    {
+        this.debug_mode = debug_mode;
+    }
+    public WhiteTiles()
+    {
+
+    }
+
+    public void changeField()
+    {
+        Random random = new Random();
+        int rnd = random.nextInt(buttons.size());
+        if(!debug_mode)
+        {
+            buttons.get(rnd).setBackgroundColor(Color.WHITE);
+        }
+        button_state.set(rnd,TileColor.WHITE);
+        for(int i = 0; i < buttons.size(); i++)
+        {
+            if(i != rnd)
+            {
+                if(!debug_mode)
+                {
+                    buttons.get(i).setBackgroundColor(Color.BLACK);
+                }
+                button_state.set(i,TileColor.BLACK);
+            }
+
+        }
+    }
+
+    public void addBtnToList(Button btn)
+    {
+        buttons.add(btn);
+        button_state.add(TileColor.WHITE);
+    }
+
+    public boolean checkCorrectTileAndCount(Button btn)
+    {
+
+        TileColor a = button_state.get(buttons.indexOf(btn));
+
+        if(button_state.get(buttons.indexOf(btn)).equals(TileColor.WHITE))
+        {
+            return false;
+        }
+        player_points++;
+        changeField();
+        return true;
+    }
+    public void restartGame()
+    {
+        player_points = 0;
+    }
+
+    public int getPlayer_points()
+    {
+        return player_points;
+    }
+
+}
--- Games/app/src/main/java/at/tugraz/ist/ma/games/WhiteTilesActivity.java
+++ Games/app/src/main/java/at/tugraz/ist/ma/games/WhiteTilesActivity.java
@@ -2,13 +2,111 @@ package at.tugraz.ist.ma.games;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class WhiteTilesActivity extends AppCompatActivity {
 
+    WhiteTiles whiteTiles = new WhiteTiles();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_white_tiles);
+
+        Button btn_1_1 = (Button) findViewById(R.id.btnWhiteTilesGameButton1_1);
+        whiteTiles.addBtnToList(btn_1_1);
+
+        Button btn_1_2 = (Button) findViewById(R.id.btnWhiteTilesGameButton1_2);
+        whiteTiles.addBtnToList(btn_1_2);
+
+        Button btn_1_3 = (Button) findViewById(R.id.btnWhiteTilesGameButton1_3);
+        whiteTiles.addBtnToList(btn_1_3);
+
+        Button btn_1_4 = (Button) findViewById(R.id.btnWhiteTilesGameButton1_4);
+        whiteTiles.addBtnToList(btn_1_4);
+
+        Button btn_2_1 = (Button) findViewById(R.id.btnWhiteTilesGameButton2_1);
+        whiteTiles.addBtnToList(btn_2_1);
+
+        Button btn_2_2 = (Button) findViewById(R.id.btnWhiteTilesGameButton2_2);
+        whiteTiles.addBtnToList(btn_2_2);
+
+        Button btn_2_3 = (Button) findViewById(R.id.btnWhiteTilesGameButton2_3);
+        whiteTiles.addBtnToList(btn_2_3);
+
+        Button btn_2_4 = (Button) findViewById(R.id.btnWhiteTilesGameButton2_4);
+        whiteTiles.addBtnToList(btn_2_4);
+
+        Button btn_3_1 = (Button) findViewById(R.id.btnWhiteTilesGameButton3_1);
+        whiteTiles.addBtnToList(btn_3_1);
+
+        Button btn_3_2 = (Button) findViewById(R.id.btnWhiteTilesGameButton3_2);
+        whiteTiles.addBtnToList(btn_3_2);
+
+        Button btn_3_3 = (Button) findViewById(R.id.btnWhiteTilesGameButton3_3);
+        whiteTiles.addBtnToList(btn_3_3);
+
+        Button btn_3_4 = (Button) findViewById(R.id.btnWhiteTilesGameButton3_4);
+        whiteTiles.addBtnToList(btn_3_4);
+
+        Button btn_4_1 = (Button) findViewById(R.id.btnWhiteTilesGameButton4_1);
+        whiteTiles.addBtnToList(btn_4_1);
+
+        Button btn_4_2 = (Button) findViewById(R.id.btnWhiteTilesGameButton4_2);
+        whiteTiles.addBtnToList(btn_4_2);
+
+        Button btn_4_3 = (Button) findViewById(R.id.btnWhiteTilesGameButton4_3);
+        whiteTiles.addBtnToList(btn_4_3);
+
+        Button btn_4_4 = (Button) findViewById(R.id.btnWhiteTilesGameButton4_4);
+        whiteTiles.addBtnToList(btn_4_4);
+
+
+        btn_1_1.setOnClickListener(v -> btnWhiteTileClick(btn_1_1));
+        btn_1_2.setOnClickListener(v -> btnWhiteTileClick(btn_1_2));
+        btn_1_3.setOnClickListener(v -> btnWhiteTileClick(btn_1_3));
+        btn_1_4.setOnClickListener(v -> btnWhiteTileClick(btn_1_4));
+
+        btn_2_1.setOnClickListener(v -> btnWhiteTileClick(btn_2_1));
+        btn_2_2.setOnClickListener(v -> btnWhiteTileClick(btn_2_2));
+        btn_2_3.setOnClickListener(v -> btnWhiteTileClick(btn_2_3));
+        btn_2_4.setOnClickListener(v -> btnWhiteTileClick(btn_2_4));
+
+        btn_3_1.setOnClickListener(v -> btnWhiteTileClick(btn_3_1));
+        btn_3_2.setOnClickListener(v -> btnWhiteTileClick(btn_3_2));
+        btn_3_3.setOnClickListener(v -> btnWhiteTileClick(btn_3_3));
+        btn_3_4.setOnClickListener(v -> btnWhiteTileClick(btn_3_4));
+
+        btn_4_1.setOnClickListener(v -> btnWhiteTileClick(btn_4_1));
+        btn_4_2.setOnClickListener(v -> btnWhiteTileClick(btn_4_2));
+        btn_4_3.setOnClickListener(v -> btnWhiteTileClick(btn_4_3));
+        btn_4_4.setOnClickListener(v -> btnWhiteTileClick(btn_4_4));
+
+
+        whiteTiles.changeField();
     }
+
+
+
+    private void btnWhiteTileClick(Button btn)
+    {
+        if(!whiteTiles.checkCorrectTileAndCount(btn))
+        {
+            Toast.makeText(this, "You lost", Toast.LENGTH_LONG).show();
+            //set points to zero
+        }
+        TextView score = (TextView) findViewById(R.id.tvWhiteTilesYourPoints);
+        //use resource File
+        score.setText("Your Points: " + whiteTiles.getPlayer_points());
+        
+    }
+
 }
