4171c5821e3a20a87739075cf9c126cae7642f90
J-J-Cyber <56450446+J-J-Cyber@users.noreply.github.com>
2020-06-03 15:23:44 +0200

    GAMES_010_B Code Optimization [SJ,DS]

--- Games/app/src/main/java/at/tugraz/ist/ma/games/SettingsActivity.java
+++ Games/app/src/main/java/at/tugraz/ist/ma/games/SettingsActivity.java
@@ -3,7 +3,6 @@ package at.tugraz.ist.ma.games;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
