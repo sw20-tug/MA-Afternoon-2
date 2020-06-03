190816fea93bb2610080d8bd966d420eb41e121a
Daniel Pollhammer <pollhammer@student.tugraz.at>
2020-06-03 14:00:00 +0200

    GAMES_010_A testcase [PD,HM]

--- Games/app/src/test/java/at/tugraz/ist/ma/games/DataManagerUnitTest.java
+++ Games/app/src/test/java/at/tugraz/ist/ma/games/DataManagerUnitTest.java
@@ -1,30 +1,47 @@
 package at.tugraz.ist.ma.games;
 
+import org.json.JSONException;
 import org.junit.Test;
 import android.content.Context;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
-
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 
 public class DataManagerUnitTest
 {
 
-//    @Test
-//    public void checkWrite() {
-//        try {
-//            String filename = "asdf.json";
-//            File file = new File(Objects.requireNonNull(System.getProperty("user.dir")) + filename);
-//            FileOutputStream stream = new FileOutputStream(file);
-//            Context test = mock(Context.class);
-//            when(test.openFileOutput(filename, Context.MODE_PRIVATE)).thenReturn(stream);
-//            DataManager.storeWordList(new String[]{"Test1", "Test2", "Test3"}, "de", test);
-//        } catch (Exception e){
-//            System.out.println(e.toString());
-//        }
-//    }
+
+    private String filename = "words_en.json";
+
+    @Test
+    public void checkWrite() throws IOException, JSONException
+    {
+
+        String[] word_list = new String[]{ "test1", "test2", "test3"};
+
+        System.out.println(System.getProperty("user.dir") + filename);
+        File file = new File(Objects.requireNonNull(System.getProperty("user.dir"))  + "/" + filename);
+        FileOutputStream outstream = new FileOutputStream(file);
+        Context context = mock(Context.class);
+        when(context.openFileOutput(filename, Context.MODE_PRIVATE)).thenReturn(outstream);
+        when(context.getString(R.string.wordfile)).thenReturn(filename);
+        DataManager.storeWordList(word_list,context);
+
+        FileInputStream instream = new FileInputStream(file);
+        when(context.openFileInput(filename)).thenReturn(instream);
+
+        String[] words = DataManager.loadWordList(context);
+        assertArrayEquals(word_list,words);
+
+
+    }
 }
